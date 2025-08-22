a74ff6efa0beada12c7880940a5aab0856cad85d
Saultube <saultube0@gmail.com>
2025-08-22 14:23:03 +0100

    few changes that break uv maps and vertex colors but can be easily fixed later

--- levels/bob/texscroll.inc.c
+++ levels/bob/texscroll.inc.c
@@ -21,7 +21,7 @@ void scroll_bob_dl_JuiceFall2_mesh_layer_1_vtx_0() {
 
 void scroll_bob_dl_MainGeometry_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 73;
+	int count = 68;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
@@ -42,7 +42,7 @@ void scroll_bob_dl_MainGeometry_mesh_layer_1_vtx_0() {
 
 void scroll_bob_dl_Purple_Juice_Box_Mesh_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 16;
+	int count = 12;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
@@ -63,7 +63,7 @@ void scroll_bob_dl_Purple_Juice_Box_Mesh_mesh_layer_1_vtx_0() {
 
 void scroll_bob_dl_Water_Box_Mesh_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 156;
+	int count = 159;
 	int width = 32 * 0x20;
 	int height = 32 * 0x20;
 
